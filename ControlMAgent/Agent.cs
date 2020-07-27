88fc1a51a0c661b0cd86af73f104289990e1cc00
unknown <mchenf@icloud.com>
2020-07-28 02:28:20 +0800

    Added a few more features, Added Server objects. Added some to string functions. Added on Node Objects, to host name entry Fixed a few minor bug, did some testing.
    Problem: Server is NOT in the same namespace as Agents and others.
    Problem: The domain name is missing the dot, check regex, or trim.
    
    Flag: Need to work on a better testing script, preferably in PowerShell
    Script.

---
+++ ControlMAgent/Agent.cs
@@ -0,0 +1,46 @@
+﻿using ControlMObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlM
+{
+    public sealed class Agent : Components
+    {
+        public Agent(ControlMVersion version, ClientNode node, Server connectedMServer, int msmaPort) : base(version, node)
+        {
+            ConnectedMServer = connectedMServer;
+            MSMAPort = msmaPort;
+        }
+
+        #region Properties
+        private Server connectedMServer;
+
+        public Server ConnectedMServer
+        {
+            get { return connectedMServer; }
+            set { connectedMServer = value; }
+        }
+
+        private int msmaPort;
+
+        public int MSMAPort
+        {
+            get { return msmaPort; }
+            set { msmaPort = value; }
+        }
+
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            //Agent name concatenation example version 7: [aur00101(uny30110):27006]
+            //Agent name concatenation example version 9: [aur00101.sony.co.jp(uny30110):27006]
+            if (Version.Major <= 7) 
+                return string.Format("[{0}({1}):{2}", Node.Machine.Hostname, ConnectedMServer.Node.Machine.Hostname, MSMAPort);
+            return string.Format("[{0}({1}):{2}", Node.Machine.FQDN, ConnectedMServer.Node.Machine.Hostname, MSMAPort);
+        }
+        #endregion
+    }
+}
---
+++ ControlMAgent/Server.cs
@@ -0,0 +1,19 @@
+﻿using ControlM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlMObjects
+{
+    public sealed class Server : Components
+    {
+        public Server(ControlMVersion version, ClientNode node) : base(version, node)
+        {
+            
+        }
+
+        #region Properties Definitions
+            
+        #endregion
+    }
+}
